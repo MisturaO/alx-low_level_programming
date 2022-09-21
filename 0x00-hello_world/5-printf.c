54692fe379b7a91ed5be29c42a5ab087c12c1a62
MisturaO <misturaojulari91@gmail.com>
2022-09-20 17:59:38 -0700

    Write a program that prints a string followed by a newline using prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of my code
+ * Return: returns 0 means code runs without error
+ * printf: prints a string followed by a newline
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
