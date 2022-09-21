c758677623fcd1af00dafc409d0054624e9f4cca
MisturaO <misturaojulari91@gmail.com>
2022-09-20 17:27:56 -0700

    Write a C program that prints a string followed by a newline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+/**
+ * main: The code entry point
+ * Returns: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
