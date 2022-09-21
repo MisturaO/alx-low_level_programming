7353d7d8fd8a9ec96ba3b4d8eaa9fe4fe19ba1e8
MisturaO <misturaojulari91@gmail.com>
2022-09-21 05:09:05 -0700

    the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,14 @@
 
 /**
  * main - Entry point
- * Return: returns 1
+ *
+ * write: prints string
+ *
+ * Return: returns 1 (success)
  */
 
 int main(void)
 {
-	write( 2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
