da5b0cfa16b1ac71deb3b8144dbb6681568fa8e8
MisturaO <misturaojulari91.com>
2022-12-08 07:21:35 -0800

    prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char c));
-	printf("size of an int: %lu byte(s)\n", sizeof(int i));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int l));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int li));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float fl));
+	printf("size of a char: %i byte(s)\n", sizeof(char c));
+	printf("size of an int: %i byte(s)\n", sizeof(int i));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int l));
+	printf("size of a long long int: %i byte(s)\n", sizeof(long long int li));
+	printf("Size of a float: %i byte(s)\n", sizeof(float fl));
 	return (0);
 }
