80733277d07f53344c33f930e801f910e4192c54
MisturaO <misturaojulari91.com>
2022-12-09 10:35:51 -0800

    prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ *Program: prints all single digit numbers of base 
+ *10 starting from 0, followed by a new line.
+ *Auth: Mistura Ojulari
+ */
+
+#include <stdio.h>
+
+/**
+  * main- Source code entry point.
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	int a;
+
+	for(a = 0; a < 10; a++)
+		printf("%d", a);
+
+	putchar('\n');
+
+	return (0);
+}
