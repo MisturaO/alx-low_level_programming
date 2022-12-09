71acce4badd1a15293defc2c5405f8d8480d1f24
MisturaO <misturaojulari91.com>
2022-12-09 12:56:49 -0800

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,35 @@
+/*
+ * Program: a program that prints all the numbers
+ * of base 16 in lowercase, followed by a new line
+ * Auth: Mistura Ojulari
+ */
+
+#include <stdio.h>
+
+/**
+ * main- Source code entry point.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char b = 'a';
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
+
+
