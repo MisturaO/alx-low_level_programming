0402743bd196d17f37dbe572f3229b9f82b27087
MisturaO <misturaojulari91.com>
2022-12-09 13:22:45 -0800

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,35 @@
+/*
+ * Program: A program that prints all possible
+ *combinations of single-digit numbers separated
+ * by commas and white space
+ *Auth: Mistura Ojulari
+ */
+
+#include <stdio.h>
+
+
+/**
+ * main- Source code entry point.
+ *Return: Always returns o.
+ */
+
+
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (a < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
