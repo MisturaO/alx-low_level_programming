ae38e80562164925b3deb199c9abdba1398484a7
MisturaO <misturaojulari91.com>
2022-12-13 10:46:49 -0800

    A function that checks for alphabetic character.

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -10,6 +10,16 @@
 
 int main(void)
 {
-	print_alphabet();
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
