d192be64c8d00d4fac3c535326aef2a113e7a638
MisturaO <misturaojulari91.com>
2022-12-14 05:02:09 -0800

    function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * Program: A function that adds two integers and returns the result.
+ * Auth: Mistura Ojulari.
+ */
+
+#include "main.h"
+
+/**
+ * add- adds two integers and returns the result.
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
