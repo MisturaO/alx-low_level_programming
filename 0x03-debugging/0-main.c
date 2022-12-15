551b1912f30239fe184ccb5bb161b6a4875e177a
MisturaO <misturaojulari91.com>
2022-12-15 06:12:41 -0800

    the function gives the correct output when given a case of 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if intege
+*	is positive or negative
 * Return: 0
 */
 
@@ -12,5 +13,5 @@ int main(void)
         i = 0;
         positive_or_negative(i);
 
-  return (0);
+	return (0);
 }
