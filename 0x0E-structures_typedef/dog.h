f264f5be335c5685dc1b83e38d51ad9cfc0253ef
MisturaO <misturaojulari91.com>
2023-01-17 06:44:53 -0800

    function that initialize a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Define a new type struct dog
@@ -15,5 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/*Prototype of the function that initializes "struct dog" structure*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
