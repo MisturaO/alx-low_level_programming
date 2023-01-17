482dd28c7aac40e3ffa98b7d3ec66057f32b181c
MisturaO <misturaojulari91.com>
2023-01-17 13:18:59 -0800

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: struct_dog variable
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
