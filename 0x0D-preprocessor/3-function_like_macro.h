eb36e2a30df000da0cde3beb15d6680b0e0af5b7
MisturaO <misturaojulari91.com>
2023-01-18 13:11:49 -0800

    macro ABS(x) that computes the absolute value of a number x.

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,7 +5,8 @@
  * File: 3-function_like_macro.h
  * Desc: function-like macro ABS(x) that computes
  *	the absolute value of a number x.
+ * @x: number to compute
  */
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
