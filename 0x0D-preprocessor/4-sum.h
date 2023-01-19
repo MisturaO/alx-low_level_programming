f0067f2cebb5c06d860e0b8315f5d96dc3ef761e
MisturaO <misturaojulari91.com>
2023-01-19 01:59:55 -0800

    computes the sum of the numbers

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,11 @@
+#ifndef SUM_H
 #define SUM_H
-#ifdef SUM_H
-
 /**
  * FILE: 4-sum.h
  * DESC: function-like macro SUM(x, y)
  *	that computes the sum of the numbers x and y.
+ * @x: first argument to compute
+ * @y: second argument to compute
  */
 
 #define SUM(x, y) ((x) + (y))
