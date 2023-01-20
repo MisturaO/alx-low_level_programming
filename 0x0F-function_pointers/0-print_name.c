0cf903c3b463bc1aaa1cb91ca337b3c5b85e0b82
MisturaO <misturaojulari91.com>
2023-01-20 00:10:41 -0800

    function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,8 @@
-#include <stdio.h>
+/**
+ * print_name - prints a name.
+ * @name: param name to print
+ * @f: prints output of other functions
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
