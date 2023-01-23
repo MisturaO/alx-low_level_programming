fc204da9d3fccd6e5c9a31444e3e9928845169ee
MisturaO <misturaojulari91.com>
2023-01-23 07:09:45 -0800

    sums up all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,14 +16,15 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	for (i = 0; i < n; i++)
-		sum+= va_arg(ap, int);
 	if (n == 0)
-		return(0);
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
 	va_end(ap);
 
-	return(sum);
+	return (sum);
 
-	return(0);
-}
+	return (0);
+sum of all its parameters.}
