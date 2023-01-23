183e785c96c0d5375277a65f31e2a08720f902e6
MisturaO <misturaojulari91.com>
2023-01-23 09:09:09 -0800

    prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(num, int));
 		if (separator != NULL && i != (n - 1))
-			printf("%c", *separator);
+			printf("%s", separator);
 	}
 	putchar('\n');
 	va_end(num);
