c969ef6b235b629aa766177021fc3e82bdef743f
MisturaO <misturaojulari91@gmail.com>
2023-02-01 05:08:49 -0800

    adds a new node at the beginning of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,12 +14,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *temp;
 
 	temp = malloc(sizeof(listint_t));
-	if (head == NULL)
+	if (temp == NULL)
 		return (NULL);
 
 	temp->next = *head;
 	temp->n = n;
 	*head = temp;
 
-	return (temp);
+	return (*head);
 }
