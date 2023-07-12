0dfd96912d249b1a16aa4c45ade1b99604bccebb
MisturaO <misturaojulari91@gmail.com>
2023-07-12 16:26:59 +0100

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  *
  * @array: input array
  * @size: size of the array
- * @value: value to search in
+ * @value: value to search in the array
  * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
